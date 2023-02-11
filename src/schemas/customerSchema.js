d0590b02cbabac970d2d2b50da08d2979b4ddb1d
Renan Damascena Pereira <renandamascena@gmail.com>
2023-02-11 16:40:40 -0300

    datas corrigidas

--- src/schemas/customerSchema.js
+++ src/schemas/customerSchema.js
@@ -4,6 +4,6 @@ export const customerSchema = Joi.object({
     name:Joi.string().min(3).required(),
     phone:Joi.string().regex(/^[0-9]+$/).min(10).max(11),
     cpf:Joi.string().regex(/^[0-9]+$/).required().min(11).max(11),
-    birthday:Joi.string().min(10).max(10)
+    birthday:Joi.date().iso().required()
 })
 
