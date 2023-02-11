0e67cd013f523a7c25efe175639218c37aae2bc1
Renan Damascena Pereira <renandamascena@gmail.com>
2023-02-11 16:12:45 -0300

    novos ajustes

--- src/schemas/customerSchema.js
+++ src/schemas/customerSchema.js
@@ -2,8 +2,8 @@ import Joi from "joi";
 
 export const customerSchema = Joi.object({
     name:Joi.string().min(3).required(),
-    phone:Joi.string().min(10).max(11),
-    cpf:Joi.string().min(11).max(11),
+    phone:Joi.string().regex(/^[0-9]+$/).min(10).max(11),
+    cpf:Joi.string().regex(/^[0-9]+$/).required().min(11).max(11),
     birthday:Joi.string().min(10).max(10)
 })
 
--- src/schemas/gameSchema.js
+++ src/schemas/gameSchema.js
@@ -3,6 +3,6 @@ import Joi from "joi";
 export const gameSchema = Joi.object({
     name:Joi.string().min(1).required(),
     image:Joi.string(),
-    stockTotal:Joi.number().min(0).required(),
-    pricePerDay:Joi.number().min(0).required()
+    stockTotal:Joi.number().min(1).required(),
+    pricePerDay:Joi.number().min(1).required()
 })
